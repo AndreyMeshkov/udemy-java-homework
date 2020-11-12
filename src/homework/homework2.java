3ebdf6f1aa4ef20ae5b4d25789a8bf391fe3a5c2
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-11-12 17:28:27 +0300

    feat: add homework2

---
+++ src/homework/homework2.java
@@ -0,0 +1,60 @@
+package homework;
+
+public class homework2 {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        byte b2 = 0b1100;
+        byte b3 = 014;
+        byte b4 = 0xc;
+        short s1 = 1300;
+        short s2 = 0b10100010100;
+        short s3 = 02424;
+        short s4 = 0x514;
+        int i1 = 0;
+        int i2 = 0b0;
+        int i3 = 00;
+        int  i4 = 0x0;
+        long l1 = 123456789;
+        long l2 = 0b111010110111100110100010101;
+        long l3 = 0726746425;
+        long l4 = 0x75BCD15;
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        float f1 = 2.5563f;
+        float f2 = 3.3542F;
+        System.out.println(f1);
+        System.out.println(f2);
+
+        double d1 = 3.94709;
+        double d2 = 2.93274;
+        System.out.println(d1);
+        System.out.println(d2);
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+        char c1 = 1;
+        char c2 = 'f';
+        char c3 = '\u003f';
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+    }
+}
